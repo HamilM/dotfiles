e3aa1091fd92343aad463c4ca24b290531f38eec
Matan Hamilis <hamilis@cs.technion.ac.il>
2018-12-15 17:14:57 +0200

    change xclip to clipboard selection mode

--- scripts/pick_emoji.sh
+++ scripts/pick_emoji.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 pushd .
 cd $(dirname ${BASH_SOURCE[0]})
-cat ./noto_emoji_map.txt | python ./rofi_emojis.py | cut -f 1 -d ':' | xclip 
+cat ./noto_emoji_map.txt | python ./rofi_emojis.py | cut -f 1 -d ':' | xclip -selection clipboard
 popd
