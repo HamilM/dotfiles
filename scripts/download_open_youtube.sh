11340f9a61c331d98a5e8ae8efc2c978489cea94
Matan Hamilis <hamilis@cs.technion.ac.il>
2019-06-23 21:45:16 +0300

    add download_open_youtube.sh works!

---
+++ scripts/download_open_youtube.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -x
+OUTPUT_DIR="~/music"
+BROWSER=firefox
+for win in $(xdotool search --all --onlyvisible --name "$BROWSER"); do
+    xdotool key --window "$win" CTRL+l CTRL+Insert
+    url=$(xclip -o | sed -r -n '/youtube.com\/watch?/p')
+    [ -n "$url" ] && break
+done
+if [ -z "$url" ]; then
+    pkill -0 dunst && notify-send "No $BROWSER window found with visible youtube tab open!"
+    exit
+fi
+#Xdialog --inputbox "Please type in the youtube url to be downloaded" 100 100 2>$FILE
+TITLE=$(youtube-dl --get-title "$url")
+youtube-dl --max-downloads 1 -x -i -f 'bestaudio' "$url" -o "$OUTPUT_DIR/%(uploader)s/%(title)s.%(etx)s"
+
+if [ $? -ne 0 ]; then
+    notify-send "Failed to download!"
+    exit
+fi
+
+pkill -0 dunst && notify-send "Song $TITLE downloaded successfully!"
+rm $FILE
